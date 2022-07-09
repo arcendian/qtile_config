a049ee4b6cc8a509d1eb2148f6c1bb0d0fc0cdf8
Michael Espeña <michael.esp42@protonmail.com>
2022-07-09 17:12:27 +0800

    adjust colors and copy nord official colors for reference

--- qcolors.py
+++ qcolors.py
@@ -10,11 +10,35 @@ nordlike = {
     "magenta": "#b48ead",
     "cyan": "#88c0d0",
     "white": "#e5e9f0",
-    "orange": "#c9826b",
+    "orange": "#d08770",
     "pink": "#bf88bc",
+    "dark-orange": "#c9826b",
     "dark-gray": "#3B4252",
     "dark-cyan": "#8FBCBB",
     "dark-magenta": "#5e81ac",
     "dark-blue": "#91acd1",
     "gray": "#434c5e",
 }
+
+nord = [
+    # Polar Night
+    "#2e3440",  # 0
+    "#3b4252",  # 1
+    "#434c5e",  # 2
+    "#4c566a",  # 3
+    # Snow Storm
+    "#d8dee9",  # 4
+    "#e5e9f0",  # 5
+    "#eceff4",  # 6
+    # Frost
+    "#8fbcbb",  # 7
+    "#88c0d0",  # 8
+    "#81a1c1",  # 9
+    "#5e81ac",  # 10
+    # Aurora
+    "#bf616a",  # 11
+    "#d08770",  # 12
+    "#ebcb8b",  # 13
+    "#a3be8c",  # 14
+    "#b48ead",  # 15
+]
